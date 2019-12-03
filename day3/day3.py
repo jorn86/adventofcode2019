f12faed440e7a12b6689cbd071c458d9264408bc
JornH <j.hertsig@topdesk.com>
2019-12-03 15:46:52 +0100

    Day 3, part 2

--- day3/day3.py
+++ day3/day3.py
@@ -1,17 +1,17 @@
 def path_right(position, length):
-    return [(position[0], i) for i in range(position[1], position[1] + length)]
+    return [(position[0], i) for i in range(position[1] + 1, position[1] + length)]
 
 
 def path_left(position, length):
-    return [(position[0], i) for i in range(position[1], position[1] - length, -1)]
+    return [(position[0], i) for i in range(position[1] - 1, position[1] - length, -1)]
 
 
 def path_up(position, length):
-    return [(i, position[1]) for i in range(position[0], position[0] + length)]
+    return [(i, position[1]) for i in range(position[0] + 1, position[0] + length)]
 
 
 def path_down(position, length):
-    return [(i, position[1]) for i in range(position[0], position[0] - length, -1)]
+    return [(i, position[1]) for i in range(position[0] - 1, position[0] - length, -1)]
 
 
 def traverse(r, position, full_path):
@@ -24,12 +24,12 @@ def traverse(r, position, full_path):
     direction = r[0:1]
     length = int(r[1:]) + 1
     path_segment = options[direction](position, length)
-    full_path.update(path_segment)
+    full_path.extend(path_segment)
     return path_segment[-1]
 
 
 def path(route):
-    full_path = set()
+    full_path = [(0, 0)]
     position = (0, 0)
     for r in route:
         position = traverse(r, position, full_path)
@@ -41,6 +41,14 @@ first = file[0].split(',')
 second = file[1].split(',')
 first_path = path(first)
 second_path = path(second)
-distances = [abs(x[0]) + abs(x[1]) for x in first_path.intersection(second_path)]
-distances.remove(0)
-print(min(distances))
+crosses = set(first_path).intersection(set(second_path))
+crosses.discard((0, 0))
+
+print(min([abs(x[0]) + abs(x[1]) for x in crosses]))  # part 1
+
+min_steps = 999999
+for c in crosses:
+    steps = first_path.index(c) + second_path.index(c)
+    if min_steps > steps:
+        min_steps = steps
+print(min_steps)  # part 2
