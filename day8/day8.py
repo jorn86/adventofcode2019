c8f06ac351170604af5a352628da52f8bfd38316
JornH <j.hertsig@topdesk.com>
2019-12-09 14:15:55 +0100

    Day 8

---
+++ day8/day8.py
@@ -0,0 +1,26 @@
+size = 25 * 6
+
+with open('./input.txt', 'r') as f:
+    inputs = [int(d) for d in f.read()]
+
+layers = [inputs[i:i + size] for i in range(0, len(inputs), size)]
+
+_min = 999
+for layer in layers:
+    zeroes = layer.count(0)
+    if zeroes < _min:
+        _min = zeroes
+        ones = layer.count(1)
+        twos = layer.count(2)
+        print('{}: {}x{}={}'.format(zeroes, ones, twos, ones * twos))
+
+
+def paint(index):
+    for layer in layers:
+        if layer[index] != 2:
+            return layer[index]
+
+
+chars = ['\u2588' if paint(i) == 1 else ' ' for i in range(0, 150)]
+for line in [''.join(chars[i:i + 25]) for i in range(0, 150, 25)]:
+    print(line)
